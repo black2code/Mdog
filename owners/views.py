68d3fa59970995bbf4638c68a4d98f917ea19ce1
MinWook Jang <jmw1080@gmail.com>
2021-12-14 21:47:58 +0900

    Django CRUD2 Mdog POST and GET

--- owners/views.py
+++ owners/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http     import JsonResponse
 from django.views    import View
 
-from owners.models import *
+from owners.models import Owner, dog
 
 class ownersView(View):
     def post(self, request):    
@@ -15,6 +15,21 @@ class ownersView(View):
             age = data['age']
         )
         return JsonResponse({'message':'created'}, status=201)
+    
+    def get(self, request):
+        owners = Owner.objects.all()
+        results = []
+        for owner in owners:
+            dogs = owner.dog_set.all().values('name', 'age')
+            results.append(
+                {
+                    "owner name"      : owner.name,
+                    "owner age"       : owner.age,
+                    "email"           : owner.email,
+                    "dog"             : [dog for dog in dogs]
+                }
+            )
+        return JsonResponse({'results':results}, status=200)
 
 
 class dogsView(View):
@@ -28,3 +43,17 @@ class dogsView(View):
             
         )
         return JsonResponse({'message':'created'}, status=201)
+    
+    def get(self, request):
+        dogs = dog.objects.all()
+        results = []
+        for dog1 in dogs:
+            results.append(
+                {
+                    "owner"     : dog1.owner.name,
+                    "name"      : dog1.name,
+                    "age"       : dog1.age
+                }
+        )
+        return JsonResponse({'results':results}, status=200)
+
